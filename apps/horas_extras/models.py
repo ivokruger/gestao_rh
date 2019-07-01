0db18715d2d8d948816a9b1455611c307cf15161
Ivo Kruger <ivobkruger@gmail.com>
2019-07-01 06:24:13 -0300

    Alteracao no model de horas extras

--- apps/horas_extras/models.py
+++ apps/horas_extras/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class HoraExtra(models.Model):
+    motivo = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.motivo
